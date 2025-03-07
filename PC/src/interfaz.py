c915bef759e5422e31fb2f9ef3651ca3856b6105
Cesar Hernandez <cesarhch1@gmail.com>
2025-03-07 20:02:30 +0100

    Update interfaz.py

--- PC/src/interfaz.py
+++ PC/src/interfaz.py
@@ -1,31 +1,44 @@
+import platform
 from tkinter import *
 from contexto import Contexto
 
 def mostrar_ventana(callback_obtener_texto):
-    #Muestra una ventana con una entrada de texto y un botón.
+    # Detectar el sistema operativo
+    sistema_operativo = platform.system()
+
+    # Muestra una ventana con una entrada de texto y un botón.
     Contexto.ventana = Tk()
     ventana = Contexto.ventana
-    ventana.geometry("+%d+%d" % (ventana.winfo_screenwidth() - 300, 0))
-    ventana.title("Lara")
+
+    # Configuraciones específicas según el sistema operativo
+    if sistema_operativo == "Darwin":  # macOS
+        ventana.geometry("+%d+%d" % (ventana.winfo_screenwidth() - 300, 0))
+        ventana.title("Lara - macOS")
+    elif sistema_operativo == "Windows":
+        ventana.geometry("300x150+%d+%d" % (ventana.winfo_screenwidth() - 300, 0))  # Derecha, arriba
+        ventana.title("Lara - Windows")
+    else:  # Linux o cualquier otro sistema
+        ventana.geometry("+%d+%d" % (ventana.winfo_screenwidth() - 300, 0))  # Derecha, arriba
+        ventana.title("Lara - Otro SO")
 
     # Crear un widget de entrada de texto
     entrada = Entry(ventana)
-    entrada.pack()
-    #entrada.bind('<Return>', lambda event: callback_obtener_texto(entrada, ventana))
-    # Limpiar el campo de entrada después de procesar el texto
+    entrada.pack(pady=10)
+
     def procesar_y_limpiar(event=None):
         callback_obtener_texto(entrada, ventana)
         entrada.delete(0, END)  # Limpia el campo de entrada
 
     entrada.bind('<Return>', procesar_y_limpiar)
+
     # Crear un botón para obtener el texto
-    boton = Button(ventana, text="Obtener texto", command=lambda: callback_obtener_texto(entrada, ventana))
-    boton.pack()
+    boton = Button(ventana, text="Obtener texto", command=lambda: (callback_obtener_texto(entrada, ventana), entrada.delete(0, END)))
+    boton.pack(pady=5)
 
     return ventana
 
 def cerrar_todo():
-    #Cierra la ventana y realiza cualquier limpieza necesaria.
+    # Cierra la ventana y realiza cualquier limpieza necesaria.
     if Contexto.ventana is not None:
         try:
             Contexto.ventana.quit()  # Detiene el ciclo de eventos
